5d623502b995e5f12b3a298a711f05f959bf74e9
manianis <manianis@yahoo.fr>
2020-07-28 17:40:32 +0100

    Create a first get Route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ let express = require('express');
 
 let app = express();
 
+let personRoute = require('./routes/person');
+
+app.use(personRoute);
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
