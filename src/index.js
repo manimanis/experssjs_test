5ddecff4d17ae3a7c1cf2ac53922bad0430d5c57
manianis <manianis@yahoo.fr>
2020-07-28 17:25:42 +0100

    Express JS - Static contents server

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.info(`server is listening on ${PORT}`));
